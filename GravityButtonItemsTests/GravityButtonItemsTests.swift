0eb008f6c4b750523b19394dac646a457f671a43
KilluaSSR <xiaotian2001@outlook.com>
2025-04-16 17:25:05 +0800

    Initial Commit

---
+++ GravityButtonItemsTests/GravityButtonItemsTests.swift
@@ -0,0 +1,17 @@
+//
+//  GravityButtonItemsTests.swift
+//  GravityButtonItemsTests
+//
+//  Created by Killua Zoldyck on 4/16/25.
+//
+
+import Testing
+@testable import GravityButtonItems
+
+struct GravityButtonItemsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
